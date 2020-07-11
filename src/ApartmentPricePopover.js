27c63178fdeab17b96ae5ec5a9eac4e324815f1b
David Bernstein <davidbernstein@David-Bernsteins-MacBook-Pro.local>
2020-07-11 11:29:25 -0400

    Apartment popup improvements

--- src/ApartmentPricePopover.js
+++ src/ApartmentPricePopover.js
@@ -5,12 +5,14 @@ import { useSelector } from 'react-redux';
 import { getSelectedApartmentUnit } from './redux/selectors';
 import CanvasJSReact from "./utils/canvasjs.react";
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+const _ = require('lodash');
 
 export default function ApartmentPricePopover() {
   const apartment = useSelector(getSelectedApartmentUnit);
 
   const prices = [...apartment.prices].sort((a,b)=> b - a);
-
+  const averagePrice = _.mean(prices.map(p=>p.price));
+  console.log(averagePrice)
   const height = "400";
   const options = {
     height,
@@ -32,15 +34,26 @@ export default function ApartmentPricePopover() {
           return { y: p.price, x: p.date };
         }),
       },
+      {
+        type: "line",
+        lineDashType: "dot",
+        markerSize: 0,
+        color: 'black',
+        dataPoints: [
+          {y: averagePrice, x: prices[0].date},
+          {y: averagePrice, x: prices[prices.length - 1].date}
+        ]
+      }
     ],
   };
 
   // Make last point red
   const lastPoint = options.data[0].dataPoints[0];
-  lastPoint.color = "red";
+  lastPoint.color = lastPoint.y > averagePrice ? "green" : "red";
+  lastPoint.markerSize = 20;
+  lastPoint.indexLabel = `$${lastPoint.y.toLocaleString('en-us')}`
 
   const currentPrice = lastPoint.y;
-  const averagePrice = prices.reduce((prev, next) => prev.price + next.price) / prices.length;
   const difference = Math.round((currentPrice - averagePrice) * 1000 / averagePrice) / 10;
   const advice = `${difference > 0 ? "Don't buy" : "Buy"}, currently ${Math.abs(difference)}% ${difference > 0 ? "above" : "under"} average`;
   return (
